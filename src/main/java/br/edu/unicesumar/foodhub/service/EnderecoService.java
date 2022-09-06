b13fd0fffa09196cc216008f4a96356eed770369
João Carvalho <joaocarvalho@unicesumar.edu.br>
2022-09-05 21:54:30 -0300

    16771 - realizando ajustes do liquibase exemplo

--- src/main/java/br/edu/unicesumar/foodhub/service/EnderecoService.java
+++ src/main/java/br/edu/unicesumar/foodhub/service/EnderecoService.java
@@ -1,8 +1,11 @@
 package br.edu.unicesumar.foodhub.service;
 
+import org.springframework.stereotype.Service;
+
 import br.edu.unicesumar.foodhub.base.CrudService;
 import br.edu.unicesumar.foodhub.domain.Endereco;
 
+@Service
 public class EnderecoService extends CrudService<Endereco> {
 
 }
