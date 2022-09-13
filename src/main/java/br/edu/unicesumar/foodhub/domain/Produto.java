ef1c03af7bc0e563a3d190c48f41c1d45f7a8693
felipe.magrinelli <felipemagrinelli@outlook.com>
2022-09-13 20:06:51 -0300

    #17034-finalizando classe de produto + necessários

--- src/main/java/br/edu/unicesumar/foodhub/domain/Produto.java
+++ src/main/java/br/edu/unicesumar/foodhub/domain/Produto.java
@@ -1,14 +1,12 @@
 package br.edu.unicesumar.foodhub.domain;
 
-import java.util.List;
+import java.math.BigDecimal;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
@@ -18,23 +16,31 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Entity
-@Table(name = "grupo")
-public class Grupo implements BaseEntity {
+@Table(name = "produto")
+public class Produto implements BaseEntity {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "S_GRUPO")
-	@SequenceGenerator(name = "S_GRUPO", sequenceName = "S_GRUPO", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "S_PRODUTO")
+	@SequenceGenerator(name = "S_PRODUTO", sequenceName = "S_PRODUTO", allocationSize = 1)
 	private Long id;
 
 	@NotEmpty
 	@Column(name = "nome", nullable = false)
 	private String nome;
 
-	@OneToMany
-	@JoinColumn(name = "id_grupo")
-	private List<Produto> produtos;
+	@NotEmpty
+	@Column(name = "descricao", nullable = false)
+	private String descricao;
+
+	@NotEmpty
+	@Column(name = "valor", nullable = false)
+	private BigDecimal valor;
+
+	@NotEmpty
+	@Column(name = "ativo", nullable = false)
+	private Boolean ativo;
 }
