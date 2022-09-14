be891bbe87ad09e3e997e026801bb16bb353721b
felipe.magrinelli <felipemagrinelli@outlook.com>
2022-09-13 22:25:14 -0300

    #17035-Criação da classe de Complemento+ pendentes

---
+++ src/main/java/br/edu/unicesumar/foodhub/repository/ProdutoComplementoRepository.java
@@ -0,0 +1,9 @@
+package br.edu.unicesumar.foodhub.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.edu.unicesumar.foodhub.domain.ProdutoComplemento;
+
+public interface ProdutoComplementoRepository extends JpaRepository<ProdutoComplemento, Long> {
+
+}
---
+++ src/main/java/br/edu/unicesumar/foodhub/service/ProdutoComplementoService.java
@@ -0,0 +1,11 @@
+package br.edu.unicesumar.foodhub.service;
+
+import org.springframework.stereotype.Service;
+
+import br.edu.unicesumar.foodhub.base.CrudService;
+import br.edu.unicesumar.foodhub.domain.ProdutoComplemento;
+
+@Service
+public class ProdutoComplementoService extends CrudService<ProdutoComplemento> {
+
+}
