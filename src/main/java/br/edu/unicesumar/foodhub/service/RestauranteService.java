b837d5a5aeafef15cf99a7c8aa726566b79049db
João Carvalho <joaocarvalho@unicesumar.edu.br>
2022-09-14 19:57:52 -0300

    #17183 - realizando correções da estrutura do projeto

--- src/main/java/br/edu/unicesumar/foodhub/service/RestauranteService.java
+++ src/main/java/br/edu/unicesumar/foodhub/service/RestauranteService.java
@@ -1,11 +1,23 @@
 package br.edu.unicesumar.foodhub.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.edu.unicesumar.foodhub.base.CrudService;
+import br.edu.unicesumar.foodhub.config.auth.Roles;
 import br.edu.unicesumar.foodhub.domain.Restaurante;
 
 @Service
 public class RestauranteService extends CrudService<Restaurante> {
+
+	@Autowired
+	private UsersService usersService;
+
+	@Override
+	protected void beforeInsert(Restaurante entity) {
+		entity.getUsers().setUsername(entity.getUsers().getEmail());
+		entity.getUsers().getRoles().add(Roles.ROLE_RESTAURANTE);
+		entity.setUsers(usersService.signUp(entity.getUsers()));
+	}
 
 }
