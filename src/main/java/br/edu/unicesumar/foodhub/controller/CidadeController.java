b7473deb2ff193a3db03d0e0022484eebca31c02
João Carvalho <joaocarvalho@unicesumar.edu.br>
2022-10-18 12:25:29 -0300

    realizando a config do cors

---
+++ src/main/java/br/edu/unicesumar/foodhub/controller/CidadeController.java
@@ -0,0 +1,13 @@
+package br.edu.unicesumar.foodhub.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.unicesumar.foodhub.base.LoadController;
+import br.edu.unicesumar.foodhub.domain.Cidade;
+
+@RestController
+@RequestMapping("/api/cidades")
+public class CidadeController extends LoadController<Cidade> {
+
+}
