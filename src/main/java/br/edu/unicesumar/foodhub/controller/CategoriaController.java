36842793ca0204553e0840a750a3acb6899133ce
João Carvalho <joaocarvalho@unicesumar.edu.br>
2022-11-09 21:16:27 -0300

    criando o service repository e controller da categoria

---
+++ src/main/java/br/edu/unicesumar/foodhub/controller/CategoriaController.java
@@ -0,0 +1,13 @@
+package br.edu.unicesumar.foodhub.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.unicesumar.foodhub.base.LoadController;
+import br.edu.unicesumar.foodhub.domain.Categoria;
+
+@RestController
+@RequestMapping("/api/categorias")
+public class CategoriaController extends LoadController<Categoria> {
+
+}
